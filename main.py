b059beab2d3e6d5afe4f076bc8edc6a0af727280
ramesses2nd <small.whale.226@gmail.com>
2015-11-13 16:08:01 +0900

    JSONのサンプルを追加

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from flask import Flask
+from flask import Flask, request, json
+from flask.ext.mysql import MySQL
+
 app = Flask(__name__)
+
+
+# mysql = MySQL()
+
+# MySQL configurations
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+# app.config['MYSQL_DATABASE_DB'] = 'DEA'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
+
+# conn = mysql.connect()
+# cursor = conn.cursor()
+
+# hard coding, sample of json
+json_data={
+	"dst_ip":"0.0.0.0",
+	"dst_port":80,
+	"dst_ctry":"JP",
+	"dst_lat":"hoge",
+	"dst_long":"hoge",
+	"src_ip":"1.0.0.0",
+	"src_port":88,
+	"src_ctry":"US",
+	"src_lat":"foo",
+	"src_long":"foo",
+	"protocol":"http",
+	"traffic":1000
+}
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
+
+@app.route("/data/", methods=['GET', 'POST'])
+def data():
+	return json.dumps(json_data,indent=4)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(host='0.0.0.0',debug=True)
